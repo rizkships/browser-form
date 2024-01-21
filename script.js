34870054cd7fcb2e5c3c80438956b996ef1d38a8
rizk <rizkdotai@gmail.com>
2024-01-21 22:17:29 +0200

    Prevent default submission

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+
+/*
+
 const form = document.querySelector('form');
 const email = document.getElementById('email');
 const country = document.getElementById('country');

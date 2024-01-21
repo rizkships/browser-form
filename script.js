d4c70e0058d4514c7429c56e79f3129eb4bff77d
rizk <rizkdotai@gmail.com>
2024-01-21 22:24:38 +0200

    remove white space from inputs

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ form.addEventListener('submit', e => {
 	checkInputs();
 });
 
+function checkInputs() {
+  // trim to remove the whitespaces
+  const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+}
 
 /*
 

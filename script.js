43c9a78d44490b369b5fb0dd13ad774efd70d6bd
rizk <rizkdotai@gmail.com>
2024-01-22 10:42:59 +0200

    Validate password confirmation

--- script.js
+++ script.js
@@ -37,7 +37,14 @@ function checkInputs() {
 		setSuccessFor(password);
 	}
 
-  
+  if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+
 }
 
 /*
